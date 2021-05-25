71c47a452ad7d98632d24c6132ae68abc43b3984
1kangyun1 <1kangyun1@gmail.com>
2021-05-25 11:22:59 -0400

    refactored socket implementation and improved look up time

--- server/onlineUsers.js
+++ server/onlineUsers.js
@@ -1,2 +1,19 @@
-const onlineUsers = [];
-module.exports = onlineUsers;
+const onlineUsers = new Set();
+
+const checkOnlineUser = (id) => {
+  return onlineUsers.has(id);
+}
+
+const removeOnlineUser = (id) => {
+  return onlineUsers.delete(id);
+}
+
+const addOnlineUser = (id) => {
+  return onlineUsers.add(id);
+}
+
+module.exports = {
+  checkOnlineUser,
+  removeOnlineUser,
+  addOnlineUser
+};
