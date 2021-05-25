87d9a47e1b4f759afc32db03816c38f615c973af
1kangyun1 <1kangyun1@gmail.com>
2021-05-25 11:22:59 -0400

    refactored socket implementation and improved look up time

---
+++ server/socketHandler.js
@@ -0,0 +1,35 @@
+const {
+  checkOnlineUser,
+  removeOnlineUser,
+  addOnlineUser
+} = require("./onlineUsers");
+
+const socketHandler = (server) => {
+  const io = require("socket.io")(server);
+
+  io.on("connection", (socket) => {
+    socket.on("go-online", (id) => {
+      if (!checkOnlineUser(id)) {
+        addOnlineUser(id);
+      }
+      // send the user who just went online to everyone else who is already online
+      socket.broadcast.emit("add-online-user", id);
+    });
+
+    socket.on("new-message", (data) => {
+      socket.broadcast.emit("new-message", {
+        message: data.message,
+        sender: data.sender,
+      });
+    });
+
+    socket.on("logout", (id) => {
+      if (checkOnlineUser(id)) {
+        removeOnlineUser(id);
+        socket.broadcast.emit("remove-offline-user", id);
+      }
+    });
+  });
+}
+
+module.exports = socketHandler;
